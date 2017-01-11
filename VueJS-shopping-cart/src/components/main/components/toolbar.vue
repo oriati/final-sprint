a4dce7b46d0d8b2b1ade645c329c05c03e7e57eb
theaarontaylor <aarontaylor613@gmail.com>
2017-01-11 19:17:38 +0200

    added a bunch of components and added nav link to main comp

---
+++ VueJS-shopping-cart/src/components/main/components/toolbar.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+
+  <section class="toolbar container below-nav">
+    <h1>toolbar Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+  // import authService from '../../services/auth.service';
+
+  export default  {
+    name: 'toolbar',
+    props: [],
+    // beforeRouteEnter ( to, from, next ) {
+    //   authService.protectRoute(next);
+    // },    
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+  .toolbar {
+
+  }
+</style>
---
+++ VueJS-shopping-cart/src/components/main/main.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+
+  <section class="main container below-nav">
+    <h1>main Component</h1>
+    <toolbar></toolbar>
+    <editor></editor>
+  </section>
+
+</template>
+
+<script lang="js">
+  // import authService from '../../../services/auth.service';
+  import toolbar from '../main/components/toolbar';
+  import editor from '../main/components/editor/editor';
+
+  export default  {
+    name: 'main',
+    props: [],
+    // beforeRouteEnter ( to, from, next ) {
+    //   authService.protectRoute(next);
+    // },    
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    components: {
+      toolbar, editor
+    }
+}
+</script>
+
+<style scoped lang="scss">
+  .main {
+
+  }
+</style>
