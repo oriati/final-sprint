cd5ad84454081214ce66f93f388f63d1e45c5ce1
Ogenbd <Ogenbd@gmail.com>
2017-01-18 18:54:02 +0200

    signup is fixed

--- VueJS-shopping-cart/src/components/toolbar/toolbar.vue
+++ VueJS-shopping-cart/src/components/toolbar/toolbar.vue
@@ -84,7 +84,6 @@
 <style scoped lang="scss">
 
   .site-headers {
-    position: fixed;
     left : 2em;
     top : 5em;
     max-width: 300px;
--- VueJS-shopping-cart/src/modules/auth/auth.module.js
+++ VueJS-shopping-cart/src/modules/auth/auth.module.js
@@ -9,36 +9,33 @@ const state = {
 };
 
 const mutations = {
-  [SIGN_IN]( state, user ) {
+  [SIGN_IN](state, user) {
     console.log('signed in!!!!!');
     state.isLoggedIn = true;
     state.user = user;
   },
-  [SIGN_OUT]( state ) {
+  [SIGN_OUT](state) {
     state.isLoggedIn = false;
     state.user = null;
   }
 }
 
 const actions = {
-  signin({commit}, {username, password} ) {
+  signin({commit}, {username, password}) {
     // return is here just so that the signin component will get a promise for a .then and .catch asynchronously
-    return Vue.http.post('http://localhost:3003/login', {username: username, password: password} )
+    return Vue.http.post('http://localhost:3003/login', { username, password })
       .then(res => res.json())
-      .then(res => {
-        commit(SIGN_IN, res.user);
-        console.log('store', state.user, res);
-        return (res);
+      .then(user => {
+        console.log('res', user);
+        commit(SIGN_IN, user);
+        localStorage.setItem('token', user.token);
+        localStorage.setItem('user', JSON.stringify(user.user));
       })
   },
   signup({commit}, user) {
-    return Vue.http.post('http://localhost:3003/data/user', user )    
+    return Vue.http.post('http://localhost:3003/data/user', user)
       .then(res => res.json())
-      .then(res => {
-        commit(SIGN_IN, res.user);
-        console.log('store', state.user, res);
-        return (res);
-      // })
+      .then(user => {
       })
   }
 }
